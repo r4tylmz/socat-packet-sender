dd65608fbe440be337f407bd29f33f4fd9943ee0
Ahmed Yılmaz <47759816+r4tylmz@users.noreply.github.com>
2020-07-13 00:47:48 +0300

    Update tcpserver.sh

--- tcpserver.sh
+++ tcpserver.sh
@@ -1,9 +1,4 @@
-# A4: TCP4 sunucu. tcpserver.sh (ya da tcpserver.bat) dosyasını çalıştıracaktır. TCP4:20002 portunu
-# dinleyecek ve gelen mesajı tcplog.txt dosyasına kaydedecektir. tcplog.txt dosyasına gelen her mesaj
-# ayrı satırlarda olmak üzere:
-# TCP--> [{mesaj}{ID}{datetime}] {datetime} şeklinde yazılması beklenmektedir. Buradaki mesaj, ID ve ilk
-# datetime parametresi A1’den A2 bilgisayarına gelen mesajın içeriğidir. İkinci datetime ise A4
-# bilgisayarının mesajı aldığı andaki datetime değeri olacaktır.
+
 
 CURRENTDATE=`date`
 i=1
